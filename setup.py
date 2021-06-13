685a922274b70560ead178abd5e84c08083ed7ce
Sergei Miami <miami@blackcrystal.net>
2021-06-14 02:36:44 +0300

    Setup.py updated

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-wiki-blackcrystal",
+    version="0.0.1",
+    author="Sergei Miami",
+    author_email="miami@blackcrystal.net",
+    description="Wiki pages in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackCrystal/python-wiki",
+    project_urls={
+        "Bug Tracker": "https://github.com/BlackCrystal/python-wiki/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
